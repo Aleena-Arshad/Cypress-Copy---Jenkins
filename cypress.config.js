8f4f0617e9ef06942a756d02c30d1f8fca26a23e
BrainX Technologies <brainxtechnologies3960@BrainXs-MacBook-Air.local>
2025-09-07 19:35:13 +0500

    Initial commit from local folder

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,20 @@
-
-
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // for Mochawesome HTML reports
+      // Mochawesome plugin for generating JSON & HTML reports
       require('cypress-mochawesome-reporter/plugin')(on);
-    },  },
+    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  },
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/reports',
+    reportDir: 'cypress/reports/${browser}', // per-browser folder
     overwrite: false,
     html: true,
     json: true,
   },
+  video: true,
+  screenshotsFolder: 'cypress/screenshots',
 });

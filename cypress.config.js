fe189b4526fad83b114e6e91d234a1617455e356
BrainX Technologies <brainxtechnologies3960@BrainXs-MacBook-Air.local>
2025-09-07 18:58:06 +0500

    Initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,10 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
- e2e: {
+  e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here if needed
     },
-    baseUrl: 'https://staging.cblevelup.com/login', // You can update this as needed
-  },
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: true,
-    json: true,
   },
 });
+
